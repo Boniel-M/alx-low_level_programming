5a40648a8a2b12312fdb3b2f1a51cb1af77e1f64
Boniel-M <bonielwanjohi@gmail.com>
2023-03-16 10:31:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printf command
+ * Return: returns a success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
