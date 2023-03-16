4ab3398fc596951cf9172dea105bb7eab3e7da2f
Boniel-M <bonielwanjohi@gmail.com>
2023-03-16 11:14:08 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Learning write function, escape using \
+ * Return: return to 1.
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
