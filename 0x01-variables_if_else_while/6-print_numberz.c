306f95ba00de8992f4a25ae577a546100f983151
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 10:42:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a main function of cha
+ * Return: The fucntion return success.
+ *
+ */
+
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
