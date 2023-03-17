af0b5ef9255c4436a4ebd6dc3903d8ff9fea6ba0
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 09:24:19 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main function execution
+ *
+ * Return: returns success.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch == 'e' || ch == 'q');
+	continue;
+
+		putchar(ch);
+	putchar("ch\n")
+		return (0);
+}
