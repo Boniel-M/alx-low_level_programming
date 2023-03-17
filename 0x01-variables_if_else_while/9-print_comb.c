334a9557bf3ac5e4defbe6114040c4112eebaa04
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 13:00:14 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - This is is a fuction of main
+ * Return: Returns successfully.
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(i + '1');
+		putchar(',');
+		putchar(' ');
+
+	}
+
+		putchar('9');
+		putchar('\n');
+		return (0);
+}
