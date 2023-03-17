23e566a5b1a038bd80bd6ee0e017d142b6137cff
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 08:33:13 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main fucntion for putchar.
- * Return value 0 for this function indicates at the code has run successfully.
+ * Return:: value 0 for this function indicates at the code has run successfully.
  *
  */
 
