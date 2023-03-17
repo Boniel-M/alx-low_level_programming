65464965f8f4462a5e3b938f6be943b0d9ad9cb8
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 13:18:52 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
     int i = 0;
 
-    while (i < 9)
+    while (i < 10)
     {
         putchar(i + '0');
-        if (i != 8)
+        if (i != 9)
         {
             putchar(',');
             putchar(' ');
