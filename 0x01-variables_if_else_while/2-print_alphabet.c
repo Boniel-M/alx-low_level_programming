f2210839ace1aa0cef38b3179765d5074acf3465
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 07:33:07 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is a main function.
+ * Return - returns value success
+ */
+
+int main(void)
+{
+	int ch;
+	for( ch = 'a'; ch <= 'z'; ch++);
+	putchar(ch);
+	return (0);
+
+}
