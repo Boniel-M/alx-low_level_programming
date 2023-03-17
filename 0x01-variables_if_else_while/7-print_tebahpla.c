5e1d1272c7618fd9d6d3620bdaf80d782f2c4495
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 11:05:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is a main function
+ * Return: returns success.
+ *
+ */
+int main(void)
+{
+	int i
+	
+	for (i = '122'; i >= '97'; i++) 
+	{
+		putchar(i);
+	}
+	return (0);
