a61314c53fa149e0f045d3752ccb65b8fa738d6f
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 12:30:57 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	putchar(i + '0');
+		putchar(i + '0');
 	}
 	for (i = 10; i < 16; i++)
 	{
-	putchar(i -10 + 'a');
+		putchar(i -10 + 'a');
 	}
 	putchar('\n');
 	return (0);
