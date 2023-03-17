8c5532768fc4935b697ed9233e834658ee40ab16
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 10:17:40 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main function for integers
+ * Return: returns value successfully.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		prinf("%d\n");
+	return (0);
+}
