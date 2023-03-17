5b41f6bab7e382059a2b4c68b27ae812811378e5
Boniel-M <bonielwanjohi@gmail.com>
2023-03-17 09:46:30 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++) {
-	for (ch == 'e' || ch = 'q');
-	continue;}
-		putchar(ch);
+	for (ch = 'a'; ch <= 'z'; ch++){
+		if (ch == 'e' || ch = 'q');
+		continue;
+	}
+	putchar(ch);
 
-	putchar("ch\n")
+	putchar("\n")
 		return (0);
 }
