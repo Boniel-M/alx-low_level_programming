8834ce3eb2d063c07d2492d6562f07bb12db36f1
Boniel-M <bonielwanjohi@gmail.com>
2023-03-19 10:42:04 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,15 @@ int main(void)
 {
 	int i, k;
 
-	for (i  = 0; i < 10; i++)
+	for (i  = 0; i < 9; i++)
 
 	{
-		for (k = i; k <= 9; k++)
+		for (k = i + 1; k <= 9; k++)
 		{
 			putchar((i % 10) + '0');
 			putchar((k % 10) + '0');
 
-			if (k == 8 && k == 9)
+			if (k != 8 || k != 9)
 				continue;
 
 		putchar(' ');
