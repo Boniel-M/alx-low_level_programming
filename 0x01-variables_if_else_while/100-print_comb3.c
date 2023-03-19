30dee68b9a33749b494955cdb457e15294e5835b
Boniel-M <bonielwanjohi@gmail.com>
2023-03-19 01:46:25 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is a main function
+ * Return: This returns sucess.
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i  = '01'; i < 10; i++) 
+	{
+		putchar(i + '01')
+	}
+	return (0);
+}
