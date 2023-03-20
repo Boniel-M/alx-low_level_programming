2cb1f88c304244e6d8eadd9b9eeb2a8f7975630c
Boniel-M <bonielwanjohi@gmail.com>
2023-03-20 00:04:55 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * Main - This function prints a combination of 
+ * possible three numbers, seperated by a comma.
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 6;+ +ii)
+	{
+		for (j = 1; j <= 7; +jj)
+		{
+			for (k = 3; k <= 8)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(k + '0');
+			if (i != 6 || j != 8 ||k != 8)
+		}
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
