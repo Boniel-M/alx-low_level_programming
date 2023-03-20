81cee94c83b4e7f0224e79c6f040c1f83723dfc7
Boniel-M <bonielwanjohi@gmail.com>
2023-03-20 00:36:16 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,27 +10,25 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i <= 6; i++)
 	{
-		for (j = 1; j <= 8; j++)
+		for (j = 1; j <= 7; j++)
 		{
 			for (k = 2; k <= 9; k++)
 			{
 				if (j > i && i < k)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(k + '0');
-			if (i != 7 || j != 8 || k != 9)
-
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		}
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 6 || j != 7 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
-
 	}
 	putchar('\n');
 	return (0);
