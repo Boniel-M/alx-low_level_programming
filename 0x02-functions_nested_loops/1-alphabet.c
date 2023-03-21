001f88233fe54f4a863b3f618211831b4cccf099
Boniel-M <bonielwanjohi@gmail.com>
2023-03-21 11:01:10 -0700

    1-putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- * print_alphabet- print lowercase alphabet a - z.
+ * print_alphabet - print lowercase alphabet a - z.
+ *
  * Return: Always 0.
  */
 void print_alphabet(void)
