4b81405b9a816838c0262dfb85a1b5e07be2008f
Boniel-M <bonielwanjohi@gmail.com>
2023-03-21 15:10:02 -0700

    addition of integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,3 +1,13 @@
-#include "main.c"
+#include "main.h"
 /**
- *
+ * add - Addition of values
+ * @v1 - computinf first value
+ * @v2 - computing second value
+ * Return: Integer value
+ */
+
+int add(int v1, int v2)
+
+{
+	return (v1 +v2);
+}
