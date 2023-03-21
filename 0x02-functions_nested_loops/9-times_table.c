32a4ba568207e713a0f8d3648e521303cf790e17
Boniel-M <bonielwanjohi@gmail.com>
2023-03-21 14:48:42 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,11 @@ void times_table(void)
 		{
 			z = x * y;
 
-			if ( z > 9)
+			if (z > 9)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				
+	
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
@@ -32,12 +32,10 @@ void times_table(void)
 						_putchar(32);
 						_putchar(32);
 				}
-				{
-					_putchar(z + '0');
-				}
-				_putchar('\n');
+				_putchar(z + '0');
 			}
-						
-		}
+				_putchar('\n');
+		}			
 	}
+
 }
