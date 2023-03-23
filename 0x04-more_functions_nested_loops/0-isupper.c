fda381b62d30ab4ab085c6cef2fb44769fa75479
Boniel-M <bonielwanjohi@gmail.com>
2023-03-23 03:12:52 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 /**
  * _isupper - checks if character is uppercase
+ *
+ * isupper - checks if character is uppercase
  *
  * @c: character to check
  *
