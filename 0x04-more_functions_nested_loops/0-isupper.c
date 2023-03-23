ca265c9282921a6dab2065bf9bb100fc6ae7652d
Boniel-M <bonielwanjohi@gmail.com>
2023-03-23 03:09:44 -0700

    checking number 0-9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _isupper - checks if character is uppercase
  *
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - checks for digit 0 to 9
+ * @c: character to check
+ * Return: returns value 1 if success, 0 otherwise
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
