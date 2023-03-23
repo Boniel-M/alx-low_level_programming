c0b6c812d8a65a355bd18bd35f55e174ce702423
Boniel-M <bonielwanjohi@gmail.com>
2023-03-23 06:22:14 -0700

    prints a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,15 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		putchar('\n');
-	}
-	int i;
 
+	if (n > 0)
+	{
+
+	int i;
 	for (i = 0; i < n; i++)
 	{
 		putchar('_');
+	}
 	}
 	putchar('\n');
 }
