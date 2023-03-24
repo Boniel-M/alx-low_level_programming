125092bfc3d60e516bb4491d81f9f24d1fb3d7bb
Boniel-M <bonielwanjohi@gmail.com>
2023-03-24 02:01:24 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,26 +20,19 @@ int main(void)
 
 			printf("FizzBuzz");
 
-
 		else if (i % 3 == 0)
 
-
 			printf("Fizz");
-
 
 		else if (i % 5 == 0)
 
 			printf("Buzz");
 
-
 		else
-
-			printf("%i ", i);
+			printf("%i", i);
 
 		if (i < 100)
-
 			printf(" ");
-
 	}
 	printf("\n");
 	return (0);
