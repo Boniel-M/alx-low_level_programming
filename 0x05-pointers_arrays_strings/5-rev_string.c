5e0616fa210e6b40f7620b3a052247fdba1470a8
Boniel-M <bonielwanjohi@gmail.com>
2023-03-27 08:08:26 -0700

    prints every character of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversse
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int rev = 0;
+
+	for (i = rev - 1; i >= 0; i++)
+	{
+		putchar(s[a]);
+	}
+}
