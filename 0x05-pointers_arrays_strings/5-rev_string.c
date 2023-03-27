f073acbe8a8660b1831729bc6b88aa1b947e5053
Boniel-M <bonielwanjohi@gmail.com>
2023-03-27 08:33:05 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,17 @@
  */
 void rev_string(char *s)
 {
+	char rv = s[0];
+	int c = 0;
 	int i;
-	int rev = 0;
 
-	for (i = rev - 1; i >= 0; i++)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		putchar(s[a]);
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 }
