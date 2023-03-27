39b04c26c82356b8aec921a065108d6e06b50b08
Boniel-M <bonielwanjohi@gmail.com>
2023-03-27 09:00:42 -0700

    even and odd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,11 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
-		_putchar('\n');
 	}
 	else if (len % 2 != 0)
 	{
 		for (i = (len - 1) / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
