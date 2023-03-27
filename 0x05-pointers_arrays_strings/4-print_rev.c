cf951e980458f84150289938a1b50868bb338220
Boniel-M <bonielwanjohi@gmail.com>
2023-03-27 07:36:32 -0700

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,15 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	int len = 0;
+	int a, b;
 
-	for (i = 0; s[i] != '\0' ; i++)
-		len++;
-	for (i = len ; i >= 0 ; i--)
-		putchar(s[i]);
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		putchar(s[a]);
+	}
 	putchar('\n');
 }
