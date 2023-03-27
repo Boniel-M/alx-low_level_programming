1411b8e802e16abb0b43da43ea31c54ef3796e7a
Boniel-M <bonielwanjohi@gmail.com>
2023-03-27 03:32:00 -0700

    Resetting a variable initial using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates value of an int
+ * @*n: is the variable of type int address of the int
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
