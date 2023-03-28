13147260fe1d35f91c63fd9ba571083057f917ae
Boniel-M <bonielwanjohi@gmail.com>
2023-03-28 08:26:01 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,13 +23,11 @@ int _atoi(char *s)
         else if (*s >= '0' && *s <= '9') {
             digit = *s - '0';
             if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10)) {
-                // overflow occurred, return the maximum or minimum value
                 return sign == -1 ? INT_MIN : INT_MAX;
             }
             num = num * 10 + digit;
         }
         else if (num > 0) {
-            // stop processing after encountering the first non-digit character
             break;
         }
         s++;
