1759864e32bab03420ed074fc1322252592131d6
Boniel-M <bonielwanjohi@gmail.com>
2023-03-28 08:21:00 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,52 @@
-#include "main.h"
+#include <limits.h>
 #include <stdio.h>
+#include "main.h"
 /**
  * _atoi - converts string into integer
  * @s: character that points
  * Return: 0 always
  *
  */
+
 int _atoi(char *s)
 {
-	int sign = 1;
-	int num = 0;
+        int sign = 1;
+        long num = 0;
 
-	while (*s != '\0')
-	{
-		if (*s == '-')
-		{
-			sign *= -1;
-		}
-		else if (*s == '+')
-		{
-			sign *= 1;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			num = num * 10 + (*s - '0');
-		}
-		else if (num > 0)
-		{
-			break;
-		}
-		s++;
-	}
-	return (sign * num);
+        while (*s != '\0')
+        {
+                if (*s == '-')
+                {
+                        if (sign == -1 || num != 0)
+                        {
+                                // overflow occurred
+                                return sign == -1 ? INT_MIN : INT_MAX;
+                        }
+                        sign = -1;
+                }
+                else if (*s == '+')
+                {
+                        if (sign == -1 || num != 0)
+                        {
+                                // overflow occurred
+                                return sign == -1 ? INT_MIN : INT_MAX;
+                        }
+                        sign = 1;
+                }
+                else if (*s >= '0' && *s <= '9')
+                {
+                        if (num > (INT_MAX - (*s - '0')) / 10)
+                        {
+                                // overflow occurred
+                                return sign == -1 ? INT_MIN : INT_MAX;
+                        }
+                        num = num * 10 + (*s - '0');
+                }
+                else if (num > 0)
+                {
+                        break;
+                }
+                s++;
+        }
+	       return (int)(sign * num);
 }
