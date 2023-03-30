2a52bb9b37308a25c1cbeaf782ef0927a7e75909
Boniel-M <bonielwanjohi@gmail.com>
2023-03-30 06:16:53 -0700

    _strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, src_len;
+	int dest_len, k;
 
 	dest_len = 0;
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
-	for (src_len = 0; src[src_len] < n && src[src_len] != '\0'; src_len++, dest_len++)
+	for (k = 0; k < n && src[k] != '\0'; k++, dest_len++)
 
 	{
-		dest[dest_len] = src[src_len];
+		dest[dest_len] = src[k];
 	}
 	dest[dest_len] = '\0';
 	return (dest);
-
 }
