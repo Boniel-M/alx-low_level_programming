b555b380353c91e5aa92aa796c5ac30cac135b1d
Boniel-M <bonielwanjohi@gmail.com>
2023-04-10 08:34:44 -0700

    program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,27 +10,18 @@ int main(int argc, char *argv[])
 {
 	int cents, num_coins = 0;
 
-	/* Check for correct number of arguments */
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	/* Parse argument as integer */
-
 	cents = atoi(argv[1]);
-
-	/* Check for negative value */
 
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
-	/* Calculate number of coins needed for each denomination */
 
 	num_coins += cents / 25;
 	cents %= 25;
@@ -41,8 +32,6 @@ int main(int argc, char *argv[])
 	num_coins += cents / 2;
 	cents %= 2;
 	num_coins += cents;
-
-	/* Print result */
 
 	printf("%d\n", num_coins);
 	return (0);
