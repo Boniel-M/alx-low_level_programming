ed0478e6907315de6a49acaa9d48f486f00fa3cd
Boniel-M <bonielwanjohi@gmail.com>
2023-04-10 04:19:56 -0700

    function that prints number of arg passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counts number of entries in the commandline
+ * @argv: array of entries in the commandline
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+		printf("%d\n", argc - 1);
+		return (0);
+}
