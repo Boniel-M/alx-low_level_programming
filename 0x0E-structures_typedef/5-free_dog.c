986d9dc8c00de73fbe8442a1008396616b66f61a
Boniel-m <bonielwanjohi@gmail.com>
2023-04-17 05:00:17 -0700

    frees dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dog
+ * @d: pointer to dog to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
