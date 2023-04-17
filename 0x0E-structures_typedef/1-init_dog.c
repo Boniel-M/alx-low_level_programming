bdf9bf7f7cf4b8f929a71543a7a826fa33d8b971
Boniel-m <bonielwanjohi@gmail.com>
2023-04-17 02:30:38 -0700

    function that defines struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,24 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
-
-#include <stdio.h>
-/**
- * struct dog - represents the combination of data types of dog
- * with different characteristics, name age and owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
-
-struct dog
-{
-        char *name;
-        float age;
-        char *owner;
-} my_dog;
-
-#endif /* DOG_H */
-
 #include <string.h>
 #include <stdio.h>
 #include "dog.h"
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
-
-#include <stdio.h>
 /**
- * struct dog - represents the combination of data types of dog
- * with different characteristics, name age and owner
+ * struct dog - function that a pointer to a struct dog variable d
+ * and three arguments, name, age and owner
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -15,6 +13,6 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+};
 
 #endif /* DOG_H */
