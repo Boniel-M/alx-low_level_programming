14731c8989624b50eed799be5eb9852641c6fdef
Boniel-m <bonielwanjohi@gmail.com>
2023-04-19 04:04:22 -0700

    Function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <string.h>
 /**
  * print_name - function that prints name
  *
  * @f: function pointer
  * @name: name to be passed to the function pointer
  *
- * Return: void
  *
  */
 void print_name(char *name, void (*f)(char *))
