304c0f12323f4c8b189ba54980f5a98894445f3e
Boniel-m <bonielwanjohi@gmail.com>
2023-04-19 23:53:02 -0700

    returns sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,8 @@
 /**
  * sum_them_all -  prints sum of all its parameters
  *
+ * @n: number of int to be passed
+ *
  * Return: sum of parameters, 0 if n is equal to 0
  */
 int sum_them_all(const unsigned int n, ...)
