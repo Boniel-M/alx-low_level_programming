a6874b3a28958a6bbdba2a0bc69e6433097d3e89
Boniel-M <bonielwanjohi@gmail.com>
2023-04-27 04:41:09 -0700

    function to print out given words

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_before_main - prints a message before main function is executed
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
