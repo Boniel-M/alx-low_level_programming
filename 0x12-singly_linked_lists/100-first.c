bac9334d8b63d5f2913441bba758206727dd58b5
Boniel-M <bonielwanjohi@gmail.com>
2023-04-27 04:50:55 -0700

    function to print out given words

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,12 @@
 #include "lists.h"
 /**
  * print_before_main - prints a message before main function is executed
+ *
+ * Description: This function is called automatically before the main
+ * function is executed, and prints a message to the standard
+ * output.
+ *
+ * Return: void
  */
 void print_before_main(void)
 {
