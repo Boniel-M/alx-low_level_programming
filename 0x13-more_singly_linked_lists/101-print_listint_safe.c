353663bc997e1bbe7b4b6230fa84d0ed3313e071
Boniel-M <bonielwanjohi@gmail.com>
2023-05-01 06:26:28 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,49 +1,54 @@
-#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
- * print_listint_safe - Prints a listint_t linked list safely
+ * print_listint_safe - Prints a linked list of integers
  *
- * @head: A pointer to the head node of the linked list.
+ * @head: Pointer to the head node of the list
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
-	size_t count;
+	size_t count = 0;
+	const listint_t *tortoise, *hare;
+	
+	tortoise = hare = head;
 
-	slow = head;
-	fast = head;
-	count = 0;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (tortoise && hare && hare->next)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
-		slow = slow->next;
-		fast = fast->next->next;
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-		if (slow == fast)
+		if (tortoise == hare)
 		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 			count++;
 
-			slow = head;
-			while (slow != fast)
+			tortoise = head;
+			while (tortoise != hare)
 			{
-				printf("[%p] %d\n", (void *)slow, slow->n);
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 				count++;
-				slow = slow->next;
-				fast = fast->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			count++;
 			break;
 		}
-	}
-	if (slow != fast)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		count++;
+	}
+	while (tortoise && tortoise != head)
+	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		count++;
+		tortoise = tortoise->next;
+	}
+	if (count == 0)
+	{
+		exit(98);
 	}
 	return (count);
 }
