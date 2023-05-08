508313ae079fcb32444c67308d906b2d3dbba3ac
Boniel-M <bonielwanjohi@gmail.com>
2023-05-08 09:21:25 -0700

    program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,17 +11,15 @@
  */
 int print_error(char *error_msg) {
     fprintf(stderr, "Error: %s\n", error_msg);
-    return (98);
+    return 98;
 }
-/**
- * print_elf_header - Prints the ELF header to stdout in the same format
- * @header: the ELF header to print
- */
+
 void print_elf_header(Elf64_Ehdr header)
 {
 	int i;
     printf("ELF Header:\n");
     printf("  Magic:   ");
+
     for (i = 0; i < EI_NIDENT; i++) {
         printf("%02x ", header.e_ident[i]);
     }
@@ -33,51 +31,4 @@ void print_elf_header(Elf64_Ehdr header)
     printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
     printf("  Type:                              %d\n", header.e_type);
     printf("  Entry point address:               0x%lx\n", header.e_entry);
-}
-/**
- * read_elf_header - Reads the ELF header from the specified file
- * @fd: the file descriptor of the ELF file
- */
-void read_elf_header(int fd) {
-    Elf64_Ehdr header;
-    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
-        exit(print_error("Failed to read ELF header"));
-    }
-
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-            header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3) {
-        exit(print_error("File is not an ELF file"));
-    }
-
-    print_elf_header(header);
-}
-/**
- * main - main entry
- *
- * @argc: the number of command-line arguments
- * @argv: the array of command-line arguments
- *
- * @return the exit status of the program
- */
-int main(int argc, char **argv)
-{
-	int fd;
-    if (argc != 2) {
-        return (print_error("Usage: elf_header elf_filename"));
-    }
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        return (print_error("Failed to open file"));
-    }
-
-    if (lseek(fd, 0, SEEK_SET) == -1) {
-        close(fd);
-        return (print_error("Failed to seek to beginning of file"));
-    }
-
-    read_elf_header(fd);
-
-    close(fd);
-    return (0);
 }
