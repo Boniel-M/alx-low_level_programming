40235dee2570d6157bb8d1627b0540e6fcf75139
Boniel-M <bonielwanjohi@gmail.com>
2023-05-26 12:12:49 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 int _putchar(char c);
