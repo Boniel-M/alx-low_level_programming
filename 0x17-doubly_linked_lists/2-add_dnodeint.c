9c69108ffb1dab5db4986a6b875c85d847127819
Boniel-M <bonielwanjohi@gmail.com>
2023-06-15 04:09:30 -0700

    Removed the fprintf function

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,24 +8,24 @@
  *
  * Return: Address of the new node, or NULL if it failed.
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node;
 
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: Unable to allocate memory for new node\n");
-	}
+		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
 	if (*head != NULL)
-	{
-		(*head)->prev = NULL;
-	}
+		(*head)->prev = new_node;
+
 	*head = new_node;
 
 	return (new_node);
